d2e1bfd65113852f19087b31f2f74c0a9840bb70
StevenLu <48115073+makise210@users.noreply.github.com>
2024-01-03 16:06:18 +0800

    Update openai.js
    can show response from openAI api

--- src/openai.js
+++ src/openai.js
@@ -1,17 +1,19 @@
-//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
+//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
 
 import OpenAI from 'openai';
 const openai = new OpenAI({
-    apiKey: "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", dangerouslyAllowBrowser: true 
+    apiKey: "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", dangerouslyAllowBrowser: true 
   });
 //get message and send request to server
-export async function sendMsgToOpenAI() {
+export async function sendMsgToOpenAI(userInput) {
     const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "You are a helpful assistant." }],
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: userInput }],
+        
         model: "gpt-3.5-turbo",
       });
     console.log(completion.choices[0].message)
-    console.log(completion.status);
     }
 
     
